d85ef4a88adc24c5f6670cd50935cc481d38de15
Chris Bishop <chris@bishoptech.io>
2022-07-29 21:24:25 -0400

    Initial Commit

---
+++ www/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    :style="image"
+    class="h-screen flex flex-col bg-cover justify-center items-center"
+  >
+    <img :src="logo" alt="Polar Bear Logo" class="h-32" /><br />
+
+    <h1 class="text-4xl text-white">Coming Soon!</h1>
+  </div>
+</template>
+
+<script>
+import bg from "static/bg.png";
+import logo from "static/bear.svg";
+
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      image: {
+        background: `url(${bg}) no-repeat center center fixed`,
+        backgroundSize: "cover",
+      },
+      logo: logo,
+    };
+  },
+};
+</script>
